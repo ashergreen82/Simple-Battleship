f9b7e8bcb3b87da8e7b481caf69cc6c79e26fe23
ashergreen82 <greena@rogers.com>
2023-06-18 16:45:56 -0400

    Refactored the code.
    I created separate functions that create a gameboard and display the gameboard.

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -8,6 +8,32 @@ namespace BattleShip
         static void Main(string[] args)
         {
             // Setup the playing board
+            Dictionary<string, string> gameBoard = GameBoardSetup();
+
+            //Display the gameboard
+            DisplayGameBoard(gameBoard);
+            Console.ReadLine();
+           
+            /*
+            string? keyboardInput = Console.ReadLine();
+            if (!String.IsNullOrEmpty(keyboardInput))
+            {
+                Console.WriteLine($"You typed: \"{keyboardInput}\". Good for you!  I am sooooo proud of you!");
+            }
+            else if (String.IsNullOrEmpty(keyboardInput))
+            {
+                Console.WriteLine("You didn't type anything, are you ok?  Is everything all right?");
+            }
+            else if (keyboardInput == null)
+            {
+                Console.WriteLine("NULL?  How the hell did you manage to return a NULL?");
+            }
+            */
+        }
+
+        public static Dictionary<string, string> GameBoardSetup()
+        {
+            // Function body goes here
             Dictionary<string, string> gameBoard = new Dictionary<string, string>();
 
             for (int j = 65; j <= 75; j++)
@@ -16,9 +42,12 @@ namespace BattleShip
                 {
                     gameBoard.Add($"{Convert.ToChar(j)}{i}", "X");
                 }
-                
             }
+            return gameBoard;
+        }
 
+        public static void DisplayGameBoard(Dictionary<string, string> gameBoard)
+        {
             // Print the column labels
             Console.Write("  "); // Space for row labels
             for (int i = 0; i <= 10; i++)
@@ -38,7 +67,7 @@ namespace BattleShip
                 {
                     string key = $"{Convert.ToChar(j)}{i}";
                     if (i >= 10)
-                    {               
+                    {
                         Console.Write($"  {gameBoard[key]} ");
                     }
                     else
@@ -48,23 +77,6 @@ namespace BattleShip
                 }
                 Console.Write("\n"); // Move to the next line
             }
-            Console.ReadLine();
-           
-            /*
-            string? keyboardInput = Console.ReadLine();
-            if (!String.IsNullOrEmpty(keyboardInput))
-            {
-                Console.WriteLine($"You typed: \"{keyboardInput}\". Good for you!  I am sooooo proud of you!");
-            }
-            else if (String.IsNullOrEmpty(keyboardInput))
-            {
-                Console.WriteLine("You didn't type anything, are you ok?  Is everything all right?");
-            }
-            else if (keyboardInput == null)
-            {
-                Console.WriteLine("NULL?  How the hell did you manage to return a NULL?");
-            }
-            */
         }
     }
 }
